9063a3352d867eb1cf74492e1eaab8f542259681
Kirill <49115928+kirill-efimov-sk@users.noreply.github.com>
2020-03-23 23:27:26 +0300

    init: add solution
    add code in file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-
+//function: min of numbers
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length == 0) {return 0;}
+  else if (array.length > 0) {let minCount = array[0];
+    for (let i = 1; i < array.length; i++) {if (array[i] < minCount) minCount = array[i];}return minCount;
+  } 
 }
 
+//function: max of numbers
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length == 0) {return 0;}
+  else if (array.length > 0) {let minCount = array[0];
+    for (let i = 1; i < array.length; i++) {if (array[i] > minCount) minCount = array[i];}return minCount;
+  } 
 }
 
+//function: avg of numbers
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length == 0) {return 0;}
+  else if (array.length > 0) {return result = (array.reduce(function(sum, current) {return (sum + current);})/array.length);}
 }
